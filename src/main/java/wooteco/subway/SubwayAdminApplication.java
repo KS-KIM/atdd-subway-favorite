49f91a3fec85d0168ddafcfdef99c3c87ffc5b31
K.S.KIM <nullable@kakao.com>
2020-07-23 20:12:35 +0900

    style: 테스트

--- src/main/java/wooteco/subway/SubwayAdminApplication.java
+++ src/main/java/wooteco/subway/SubwayAdminApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SubwayAdminApplication {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworld!");
         SpringApplication.run(SubwayAdminApplication.class, args);
     }
 }
